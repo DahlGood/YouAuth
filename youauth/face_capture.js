f60b1f1c80ceb85e6581e7bf869223856e04bae4
pete5431 <scholarpetez@gmail.com>
2020-11-25 14:20:20 -0500

    Fixed resolution of the image taken.
    -Now returns the dataURL.

--- youauth/face_capture.js
+++ youauth/face_capture.js
@@ -3,7 +3,6 @@ class FaceCapture{
   constructor(constraints, video){
     this.constraints = constraints;
     this.video = video;
-    this.imageCapture = null;
   }
 
   // start the video stream.
@@ -13,10 +12,8 @@ class FaceCapture{
       videoStream = await navigator.mediaDevices.getUserMedia(this.constraints);
       // Set the src of the video as the video stream.
       this.video.srcObject = videoStream;
+      // Play the video.
       this.video.play();
-
-      const track = videoStream.getVideoTracks()[0];
-      this.imageCapture = new ImageCapture(track);
 
     } catch(err) {
       console.log(err);
@@ -24,21 +21,20 @@ class FaceCapture{
   }
 
   // Draws the image capture on to a canvas.
-  drawCanvas(canvas, imageBitmap) {
-    canvas.width = 630;
-    canvas.height = 500;
-    canvas.getContext('2d').clearRect(0,0,canvas.width, canvas.height);
-    canvas.getContext('2d').drawImage(imageBitmap, 0, 0, canvas.width, canvas.height);
+  drawCanvas(canvas, video) {
+    canvas.width = this.constraints.video.width;
+    canvas.height = this.constraints.video.height;
+    // Clear the canvas.
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    // Draw the video onto the canvas.
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
   }
 
-  // Show the captured image on a canvas.
-  showCapture(canvas) {
-    this.imageCapture.takePhoto()
-    .then(blob => createImageBitmap(blob))
-    .then(imageBitmap => {
-      this.drawCanvas(canvas, imageBitmap);
-    })
-    .catch(err => console.log(err));
+  // Take a picture of the current video.
+  takePicture(canvas){
+    this.drawCanvas(canvas, this.video);
+    // Return data URL of the loaded image.
+    return canvas.toDataURL('image/jpg');
   }
 }
 
@@ -53,7 +49,6 @@ function testFunction() {
   }
 
   var video = document.querySelector('video');
-  var imageBitmap;
   var capture = document.querySelector('#capture');
   var canvas = document.querySelector('#display');
 
@@ -62,7 +57,8 @@ function testFunction() {
   faceCapture.startStream();
 
   capture.addEventListener("click", function(){
-    faceCapture.showCapture(canvas);
+    dataURL = faceCapture.takePicture(canvas);
+    console.log(dataURL);
   });
 }
 

8f05050285d61854fae6a7dc0ded114e2db50508
Peter Zheng <scholarpetez@gmail.com>
2020-11-28 01:07:08 -0500

    Changed faceCapture class implementation.
    -Deleted models folder in test_youauth.

--- youauth/face_capture.js
+++ youauth/face_capture.js
@@ -1,67 +1,38 @@
-class FaceCapture{
+function FaceCapture(constraints, video){
+  this.constraints = constraints;
+  this.video = video;
+}
 
-  constructor(constraints, video){
-    this.constraints = constraints;
-    this.video = video;
-
-  }
-
-  // start the video stream.
-  async startStream(){
-    let videoStream = null;
-    try {
-      videoStream = await navigator.mediaDevices.getUserMedia(this.constraints);
-      // Set the src of the video as the video stream.
-      this.video.srcObject = videoStream;
-      // Play the video.
-      this.video.play();
-
-    } catch(err) {
-      console.log(err);
-    }
-  }
-
-  // Draws the image capture on to a canvas.
-  drawCanvas(canvas, video) {
-    canvas.width = this.constraints.video.width;
-    canvas.height = this.constraints.video.height;
-    // Clear the canvas.
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-    // Draw the video onto the canvas.
-    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-  }
-
-  // Take a picture of the current video.
-  takePicture(){
-    const canvas = document.createElement("canvas");
-    this.drawCanvas(canvas, this.video);
-    // Return data URL of the loaded image.
-    return canvas.toDataURL('image/jpg');
+FaceCapture.prototype.startStream = async function startStream(){
+  let videoStream = null;
+  try {
+    videoStream = await navigator.mediaDevices.getUserMedia(this.constraints);
+    // Set the src of the video as the video stream.
+    this.video.srcObject = videoStream;
+    // Play the video.
+    this.video.play();
+  } catch(err) {
+    console.log(err);
   }
 }
 
-// Example of how to use.
-function testFunction() {
+FaceCapture.prototype.drawCanvas = function drawCanvas(canvas, video){
+  // Set canvas width and height to be the same as video.
+  canvas.width = this.constraints.video.width;
+  canvas.height = this.constraints.video.height;
+  // Clear the canvas.
+  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  // Draw the video onto the canvas.
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+}
 
-  const constraints = {
-    video: {
-      width: 630,
-      height: 500,
-    },
-  }
-
-  var video = document.querySelector('video');
-  var capture = document.querySelector('#capture');
-  var canvas = document.querySelector('#display');
-
-  faceCapture = new FaceCapture(constraints, video);
-
-  faceCapture.startStream();
-
-  capture.addEventListener("click", function(){
-    dataURL = faceCapture.takePicture(canvas);
-    console.log(dataURL);
-  });
+FaceCapture.prototype.takePicture = function takePicture(){
+  // Create new canvas element.
+  const canvas = document.createElement("canvas");
+  // Draw video onto canvas.
+  this.drawCanvas(canvas, this.video);
+  // Return data URL of the loaded image.
+  return canvas.toDataURL('image/jpg');
 }
 
 module.exports = {FaceCapture: FaceCapture};
